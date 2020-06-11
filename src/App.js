4f1562ec4283065ace15f777f01f1e765e5ad77b
cosyer <chenyu@mydearest.cn>
2020-06-11 15:23:41 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
+
+class App extends Component {
+  componentDidMount() {
+    console.log('哈哈哈~ 服务器渲染成功了！');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
