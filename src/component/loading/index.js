47358c17f7610c68e543273b9b48f30be39f32f1
cosyer <chenyu@mydearest.cn>
2020-06-16 13:55:59 +0800

    add: memo对函数式组件优化、代码拆分

--- src/component/loading/index.js
+++ src/component/loading/index.js
@@ -1,15 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import "./index.css";
 import { Spin } from "antd";
 
-class Loading extends Component {
-  render() {
-    return (
-      <div className="example">
-        <Spin />
-      </div>
-    );
-  }
+function Loading() {
+  return (
+    <div className="example">
+      <Spin />
+    </div>
+  );
 }
 
-export default Loading;
+export default React.memo(Loading);
